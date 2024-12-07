1e116b4b49edb5251c42e3cf4dea0d4cc99a06f3
akashsingh_bhandari <asbhandari14@gmail.com>
2024-12-07 21:35:48 +0530

    change again

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-// app.use(cors({
-//     origin: "*",
-//     methods: ["POST", "PUT", "GET", "DELETE"],
-//     credentials: true
-// }));
+app.use(cors({
+    origin: "https://login-signup-auth-backend.onrender.com/",
+    methods: ["POST", "PUT", "GET", "DELETE"],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
