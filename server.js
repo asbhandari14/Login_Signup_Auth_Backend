39d7ab4afb76ac6567dff368947991383ef2d144
akashsingh_bhandari <asbhandari14@gmail.com>
2024-12-07 21:12:04 +0530

    front url in the cors

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-// app.use(cors({
-//     origin: "*",
-//     methods: ["POST", "PUT", "GET", "DELETE"],
-//     credentials: true
-// }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["POST", "PUT", "GET", "DELETE"],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
